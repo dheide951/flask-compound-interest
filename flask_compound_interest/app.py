bbbfb74310e5b9e85c391be7f0f1f9f15b0d7cbf
Dawson Heide <dheide@Dawsons-iMac.local>
2018-10-07 00:02:27 -0400

    completed

--- flask_compound_interest/app.py
+++ flask_compound_interest/app.py
@@ -8,16 +8,35 @@ app = Flask(__name__)
 def form():
     result = None
     if request.method == 'POST':
-        # Get the result of the compound interest function
-        pass
+        principal = request.form['principal']
+        interest = request.form['interest']
+        numYears = request.form['years']
+        timesPerYear = request.form['times_per_year']
+
+        if principal != '' and interest != '' and numYears != '' and timesPerYear != '':
+            principal = int(principal)
+            interest = float(interest)
+            numYears = int(numYears)
+            timesPerYear = int(timesPerYear)
+            result = calcCompoundInterest(principal, interest, numYears, timesPerYear)
+
+        if result is None:
+            result = "Error, need all parameters"
+            
+
     # Pass the result to the front end
-    return render_template('form.html')
+    return render_template('form.html', result=result)
 
 
 #
 # Write a compound interest function.
 #
+def calcCompoundInterest(principal, interest, numYears, timesPerYear):
+    return principal * (1 + (interest / timesPerYear)) ** numYears
+
+
 
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
+
